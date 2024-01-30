0c33d89e3c51ab4374464c1e92d7d727f5dc8fd2
saran zafar <131322772+saranzafar@users.noreply.github.com>
2024-01-30 22:43:26 +0500

    initial commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,6 @@
+function App() {
+  return (
+    <h1>chay or react | saran</h1>
+  );
+}
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,6 @@
+function App() {
+  return (
+    <h1>chay or react with viet | saran</h1>
+  )
+}
+export default App
