09c6b534f2dcbb5da7ebc9e2edcf5ee0b145cd35
saran zafar <131322772+saranzafar@users.noreply.github.com>
2024-02-19 10:32:28 +0500

    dark/light practice, a bug in 09 porject fixed

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,17 +1,37 @@
+import { useEffect, useState } from "react"
 import './App.css'
-import UserContextProvider from './context/UserContextProvider'
-import Login from './components/Login'
-import Profile from './components/Profile'
+import { ThemeProvider } from "./contexts/theme"
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
+  const [themeMode, setThemeMode] = useState('light')
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light', 'dark')
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
 
   return (
-    <UserContextProvider>
-      <h1>Chay or Context API</h1>
-      <Login />
-      <Profile />
-    </ UserContextProvider>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   )
 }
-
 export default App
--- practice/src/contexts/theme.js
+++ practice/src/contexts/theme.js
@@ -1,16 +1,13 @@
 import { createContext, useContext } from "react";
 
 export const ThemeContext = createContext({
-    themeMode: "light",
+    thmeMode: "light",
     darkTheme: () => { },
     lightTheme: () => { },
 })
 
 export const ThemeProvider = ThemeContext.Provider
-//we are accessing the "ThemeContext" with "ThemeProvider"
 
 export default function useTheme() {
-    //just  a hook to get the context values in any component that is wrapped by <ThemeProvider /> 
-    // Custom hook to access the context
     return useContext(ThemeContext)
 }
