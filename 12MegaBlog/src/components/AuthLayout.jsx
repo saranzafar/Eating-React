c43ab62e15fc7d61ee676c0a5d7f4edc2dae8f38
saran zafar <131322772+saranzafar@users.noreply.github.com>
2024-03-01 19:42:03 +0500

    some components created and authentication of login functionality created.

---
+++ 12MegaBlog/src/components/AuthLayout.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+import { UseSelector, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+export default function Protected({ children, authentication = true }) {
+
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
+    const authStatus = useSelector(state => state.auth.status)
+
+    useEffect(() => {
+        // make it more easy 
+        if (authentication && authStatus !== authentication) {
+            navigate('/login')
+        } else if (!authentication && authStatus !== authentication) {
+            navigate('/')
+        }
+        setLoader(false)
+    }, [authStatus, navigate, authentication])
+    return ? 
+}
---
+++ 12MegaBlog/src/components/PostCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import appwriteSercice from '../appwrite/config'
+import { Link } from 'react-redux-dom'
+
+function PostCard({ $id, title, featuredImage }) {
+
+    return (
+        <link to={`/post/${$id}`}></link>
+        <div className='w-full bg-gray-100 rounded-xl p-4'>
+            <div className='w-full justify-content-center  mb-4'>
+                <img src={appwriteSercice.getFilePreview(featuredImage)} alt={title} className="rounded-xl" />
+            </div>
+            <h2
+            className='text-xl font-bold'
+            >{title}</h2>
+        </div>
+    )
+}
+
+export default PostCard
