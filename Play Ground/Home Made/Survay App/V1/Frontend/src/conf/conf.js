6fd06258af254d4482db4ba88bac7612ef836f43
saran zafar <131322772+saranzafar@users.noreply.github.com>
2024-05-18 16:48:50 +0500

    setting for deployment

--- Play Ground/Home Made/Survay App/V1/Frontend/src/conf/conf.js
+++ Play Ground/Home Made/Survay App/V1/Frontend/src/conf/conf.js
@@ -1,5 +1,6 @@
 const conf = {
-    databaseUrl: String(import.meta.env.URL || "http://localhost:8000/api/v1/")
+    // databaseUrl: String(import.meta.env.URL || "http://localhost:8000/api/v1/")
+    databaseUrl: String(import.meta.env.URL || "https://survey-app-api-umber.vercel.app/")
 }
 
 export default conf
