f7e80319a7cc3773a2f921dcb16a092b526ee886
saran zafar <131322772+saranzafar@users.noreply.github.com>
2024-05-18 17:39:49 +0500

    Update app.js

--- Play Ground/Home Made/Survay App/V1/server/src/app.js
+++ Play Ground/Home Made/Survay App/V1/server/src/app.js
@@ -9,28 +9,12 @@ import responseRoutes from "./routes/response.routes.js"
 
 const app = express();
 
-// Middleware to log requests for debugging
-app.use((req, res, next) => {
-    console.log(`Incoming request: ${req.method} ${req.url}`);
-    console.log(`Request origin: ${req.headers.origin}`);
-    next();
-});
-
 // CORS configuration for deployment
 app.use(cors({
-    origin: "https://survey-app-sage.vercel.app", // Set the allowed origin
+    origin: "*", // Set the allowed origin
     methods: ["GET", "POST", "DELETE"],
     credentials: true // Allow credentials (cookies, authorization headers, TLS client certificates)
 }));
-
-// Manually handle preflight requests for better control
-app.options('*', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "https://survey-app-sage.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.sendStatus(200);
-});
 
 // Using built-in Express middleware for parsing JSON and URL-encoded bodies
 app.use(express.json({ limit: "16kb" }));
