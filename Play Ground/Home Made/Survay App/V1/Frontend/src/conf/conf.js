4b5d01335eb1d8ea9a212ac23cd910a72916921a
saran zafar <131322772+saranzafar@users.noreply.github.com>
2024-05-19 10:15:55 +0500

    Update conf.js

--- Play Ground/Home Made/Survay App/V1/Frontend/src/conf/conf.js
+++ Play Ground/Home Made/Survay App/V1/Frontend/src/conf/conf.js
@@ -1,6 +1,6 @@
 const conf = {
     // databaseUrl: String(import.meta.env.URL || "http://localhost:8000/api/v1/")
-    databaseUrl: String(import.meta.env.URL || "https://survey-app-api-umber.vercel.app/")
+    databaseUrl: String(import.meta.env.URL || "https://survey-app-backend-liard.vercel.app/api/v1/")
 }
 
 export default conf
