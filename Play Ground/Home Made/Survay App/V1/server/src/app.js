bdcf95702563a314ed0578289679b348b0309741
saran zafar <131322772+saranzafar@users.noreply.github.com>
2024-05-19 08:43:42 +0500

    updated server for deployment

--- Play Ground/Home Made/Survay App/V1/server/src/app.js
+++ Play Ground/Home Made/Survay App/V1/server/src/app.js
@@ -10,11 +10,13 @@ import responseRoutes from "./routes/response.routes.js";
 const app = express();
 
 // CORS configuration for deployment
-app.use(cors({
-    origin: process.env.CORS_ORIGIN || "*", // Secure this for production
-    methods: ["GET", "POST", "DELETE"],
-    credentials: true
-}));
+app.use(cors(
+    {
+        origin: "*",
+        methods: ["POST", "GET", "DELETE"],
+        credentials: true
+    }
+));
 
 // Using built-in Express middleware for parsing JSON and URL-encoded bodies
 app.use(express.json({ limit: "16kb" }));
@@ -28,8 +30,9 @@ app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/admin", adminRoutes);
 app.use("/api/v1/response", responseRoutes);
 
+// Adding a GET route for the root URL
 app.get("/", (req, res) => {
-    res.json("Hello");
-})
+    res.send("hello");
+});
 
 export { app };
