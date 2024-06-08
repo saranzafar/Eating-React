17594e2e0196b8f4e5d786d794fb8ff4d04145ed
saranzafar <saranzafar736@gmail.com>
2024-06-08 09:47:23 -0700

    recover surveyApp code, changing file structure

--- Play Ground/Home Made/Survay App/V1/Frontend/src/conf/conf.js
+++
@@ -1,6 +0,0 @@
-const conf = {
-    // databaseUrl: String(import.meta.env.URL || "http://localhost:8000/api/v1/")
-    databaseUrl: String(import.meta.env.URL || "https://survey-app-backend-liard.vercel.app/api/v1/")
-}
-
-export default conf
